902214921d4ac560da5a3e30f5e55e168311c3f0
Lev <levberezov01@gmail.com>
2023-01-31 15:21:39 +0200

    dto create

--- src/main/java/mate/academy/controller/ProductController.java
+++ src/main/java/mate/academy/controller/ProductController.java
@@ -4,22 +4,37 @@ import mate.academy.model.Product;
 import mate.academy.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
     public final ProductService productService;
-
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
 
-    @GetMapping("/product")
+    @GetMapping("/")
     public List<Product> getAllProduct() {
         return productService.getAll();
+    }
+
+    @GetMapping("/inject")
+    public String injectMockData() {
+        Product iPhone = new Product();
+        iPhone.setName("iPhone 1000");
+        Product iPhone2 = new Product();
+        iPhone.setName("iPhone 1000");
+        iPhone.setPrise(BigDecimal.valueOf(94540));
+        iPhone2.setName("iPhone 2000");
+        iPhone2.setPrise(BigDecimal.valueOf(97846845));
+        productService.add(iPhone);
+        return "Done!";
     }
 }
